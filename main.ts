a6dd26c25a3eab6b6bffda7f5bf4ec0ef811ffbc
ChaosCrusher08 <92798767+ChaosCrusher08@users.noreply.github.com>
2021-12-02 09:36:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 let pressure_sensor = 0
 radio.setGroup(1)
 basic.forever(function () {
+    pins.setAudioPin(AnalogPin.P1)
     pressure_sensor = pins.analogReadPin(AnalogPin.P0)
     if (pressure_sensor > 0) {
         radio.sendString("Burger")
+        for (let index = 0; index < 100000000000000000000; index++) {
+            music.playMelody("C5 A B G A G E F ", 500)
+        }
     }
 })
