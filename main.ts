689cccf3e5ebdfa704a7388f5b7faa73ad388564
ChaosCrusher08 <92798767+ChaosCrusher08@users.noreply.github.com>
2021-12-02 08:46:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ basic.forever(function () {
     pressure_sensor = pins.analogReadPin(AnalogPin.P0)
     if (pressure_sensor > 0) {
         radio.sendNumber(1)
-        for (let index = 0; index < 1e+21; index++) {
-            music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        for (let index = 0; index < 10; index++) {
+            music.playMelody("C5 A B G A G E F ", 500)
         }
     }
 })
